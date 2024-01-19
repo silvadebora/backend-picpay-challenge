e48b64ac212a342c847841bb26d1b5841e0ca4a8
Debora <silvadeborafisio@gmail.com>
2024-01-19 10:48:20 -0300

    feat: update User

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,4 +28,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User (UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.document = data.document();
+        this.password = data.password();
+        this.email = data.email();
+    }
 }
